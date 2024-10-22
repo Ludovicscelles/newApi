353cf1a8bf366f590b230f71a69c5c23ed57a0c4
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-10-22 09:01:26 +0200

    middleware repos

--- api/src/repos/repos.controller.ts
+++ api/src/repos/repos.controller.ts
@@ -1,8 +1,27 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import repos from "../../data/repo.json";
 import type { Repo } from "./repos.type";
+import Joi from "joi";
 
 const repoControllers = express.Router();
+
+const schema = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().required(),
+  url: Joi.string().required(),
+  isPrivate: Joi.number().min(1).max(2).required(),
+  isFork: Joi.number().min(1).max(2).required(),
+});
+
+const validateRepo = (req: Request, res: Response, next: NextFunction) => {
+  const { error } = schema.validate(req.body);
+
+  if (error === null) {
+    next();
+  } else {
+    res.status(422).json(error);
+  }
+};
 
 repoControllers.get("/", (_: Request, res: Response) => {
   res.status(200).json(repos);
@@ -18,7 +37,7 @@ repoControllers.get("/:id", (req: Request, res: Response) => {
   }
 });
 
-repoControllers.post("/", (req: Request, res: Response) => {
+repoControllers.post("/", validateRepo, (req: Request, res: Response) => {
   const repo: Repo = req.body;
   repos.push(repo);
   res.status(201).json(repo);
